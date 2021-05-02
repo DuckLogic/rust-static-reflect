cfd84aa32874960ab24ab49490157b2c1b954866
Techcable <Techcable@techcable.net>
2021-05-02 13:39:37 -0700

    Fix unsafe impl in the define_extern_type macro
    Release 0.1.4

--- src/macros.rs
+++ src/macros.rs
@@ -3,9 +3,9 @@
 /// See [TypeInfo::Extern](crate::types::TypeInfo::Extern)
 #[macro_export]
 macro_rules! define_extern_type {
-    ($target:ident) => (define_extern_type!($target => $target););
+    ($target:ident) => ($crate::define_extern_type!($target => $target););
     ($target:ty => $defined_path:path) => {
-        impl ::static_reflect::StaticReflect for $target {
+        unsafe impl ::static_reflect::StaticReflect for $target {
             const TYPE_INFO: $crate::TypeInfo<'static> = $crate::TypeInfo::Extern {
                 name: stringify!($defined_path)
             };
