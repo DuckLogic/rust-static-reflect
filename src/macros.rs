63b9a03b2acf3eae5e080fd69d5fb594e1d922fe
Techcable <Techcable@techcable.net>
2021-05-02 13:06:48 -0700

    Add an define_extern_type! macro
    Replacement for DuckASM's old define_opaque_type!

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,17 @@
+/// Define a type's implementation of [StaticReflect](crate::StaticReflect) as an extern type
+///
+/// See [TypeInfo::Extern](crate::types::TypeInfo::Extern)
+#[macro_export]
+macro_rules! define_extern_type {
+    ($target:ident) => (define_extern_type!($target => $target););
+    ($target:ty => $defined_path:path) => {
+        impl ::static_reflect::StaticReflect for $target {
+            const TYPE_INFO: $crate::TypeInfo<'static> = $crate::TypeInfo::Extern {
+                name: stringify!($defined_path)
+            };
+        }
+    };
+}
 
 /// Get the integer offset of the specified field
 #[macro_export]
